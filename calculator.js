d4894eb00c684ed49de6578f213d7be38120966e
Jordan Leslie <this.codedev@gmail.com>
2017-08-28 15:19:44 -0500

    Added functionality for the equal button

--- calculator.js
+++ calculator.js
@@ -6,16 +6,6 @@
 var leftOperand = document.getElementById("left-operand");
 var rightOperand = document.getElementById("right-operand");
 var operatorVar = document.getElementById("operatorId");
-var btn1 = document.getElementById("btn1");
-var btn2 = document.getElementById("btn2");
-var btn3 = document.getElementById("btn3");
-var btn4 = document.getElementById("btn4");
-var btn5 = document.getElementById("btn5");
-var btn6 = document.getElementById("btn6");
-var btn7 = document.getElementById("btn7");
-var btn8 = document.getElementById("btn8");
-var btn9 = document.getElementById("btn9");
-var btn0 = document.getElementById("btn0");
 var btnPlus = document.getElementById("btn-plus");
 var btnMinus = document.getElementById("btn-minus");
 var btnMultiply = document.getElementById("btn-multiply");
@@ -80,18 +70,20 @@ var clear = function () {
     var result = function () {
         switch (operatorVar.value) {
             case "+":
-                console.log(parseInt(leftValue) + parseInt(rightValue));
+                result = (parseInt(leftOperand.value) + parseInt(rightOperand.value));
                 break;
             case "-":
-                // console.log(parseInt(leftValue) - parseInt(rightValue));
+                result = (parseInt(leftOperand.value) + parseInt(rightOperand.value));
                 break;
             case "*":
-                result = parseInt(leftValue) * parseInt(rightValue);
+                result = parseInt(leftOperand.value) * parseInt(rightOperand.value);
                 break;
             case "/":
-                result = parseInt(leftValue) / parseInt(rightValue);
+                result = parseInt(leftOperand.value) / parseInt(rightOperand.value);
                 break;
         }
+        clear();
+        leftOperand.value = result;
     }
 
     btnEqual.addEventListener("click", result, false);
